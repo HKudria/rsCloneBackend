752ca94c33640f8c18705be5e93f7a64f6193a5b
Herman Kudria <h.kudrya@hotmail.com>
2023-02-03 21:00:15 +0100

    feat: simple endpoint

---
+++ app.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+const User = require("./model/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const app = express();
+
+app.use(express.json());
+
+module.exports = app;
+
+app.post("/register", async (req, res) => {
+    let encryptedPassword;
+    try {
+        const {first_name, last_name, email, password} = req.body;
+
+        if (!(email && password && first_name && last_name)) {
+            res.status(409).send("All input is required");
+        }
+
+        const oldUser = await User.findOne({email});
+
+        if (oldUser) {
+            return res.status(409).send("User Already Exist. Please Login");
+        }
+        encryptedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            first_name,
+            last_name,
+            email: email.toLowerCase(), // sanitize: convert email to lowercase
+            password: encryptedPassword,
+        });
+        user.token = jwt.sign(
+            {user_id: user._id, email},
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "2h",
+            }
+        );
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!(email && password)) {
+            res.status(409).send("All input is required");
+        }
+
+        const user = await User.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            user.token = jwt.sign(
+                {user_id: user._id, email},
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "2h",
+                }
+            );
+            res.status(200).json(user);
+        } else{
+            res.status(403).send("Invalid Credentials");
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
