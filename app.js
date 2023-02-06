ec209b6abf8079bbad4d8d689952e2df18af7b29
Herman Kudria <h.kudrya@hotmail.com>
2023-02-06 18:17:23 +0100

    feat: fix login and user endpoint

--- app.js
+++ app.js
@@ -6,71 +6,74 @@ const User = require("./model/user");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const auth = require("./middleware/auth");
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 
 module.exports = app;
 
-app.post("/register", async (req, res) => {
+app.post("/register", cors(),async (req, res) => {
     let encryptedPassword;
     try {
         const {first_name, last_name, email, password} = req.body;
 
         if (!(email && password && first_name && last_name)) {
-            res.status(409).send("All input is required");
+            return res.status(409).send({'error': 'error.allFieldRequired'});
         }
 
         const oldUser = await User.findOne({email});
 
         if (oldUser) {
-            return res.status(409).send("User Already Exist. Please Login");
+            return res.status(409).send({'error': 'error.userExist'});
         }
         encryptedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({
+        await User.create({
             first_name,
             last_name,
-            email: email.toLowerCase(), // sanitize: convert email to lowercase
+            email: email.toLowerCase(),
             password: encryptedPassword,
         });
-        user.token = jwt.sign(
-            {user_id: user._id, email},
-            process.env.TOKEN_KEY,
-            {
-                expiresIn: "2h",
-            }
-        );
-        res.status(200).json(user);
+        return res.status(200).json('success');
     } catch (err) {
         console.log(err);
     }
 });
 
-app.post("/login", async (req, res) => {
+app.post("/login", cors(), async (req, res) => {
     try {
         const { email, password } = req.body;
         if (!(email && password)) {
-            res.status(409).send("All input is required");
+            return res.status(409).send({'error': 'error.allFieldRequired'});
         }
 
         const user = await User.findOne({ email });
 
         if (user && (await bcrypt.compare(password, user.password))) {
-            user.token = jwt.sign(
+            const responseUser = {
+                id: user._id,
+                fName: user.first_name,
+                lName: user.last_name,
+                email: user.email,
+            }
+            responseUser.token = jwt.sign(
                 {user_id: user._id, email},
                 process.env.TOKEN_KEY,
                 {
-                    expiresIn: "2h",
+                    expiresIn: parseInt(process.env.TOKEN_EXPIRE)+"h",
                 }
             );
-            res.status(200).json(user);
+            responseUser.expire = process.env.TOKEN_EXPIRE
+            res.status(200).json(responseUser);
         } else{
-            res.status(403).send("Invalid Credentials");
+            return res.status(403).send({'error': 'error.invalidCredential'});
         }
     } catch (err) {
         console.log(err);
     }
 });
-app.post("/welcome", auth, (req, res) => {
-    res.status(200).send("Hello world");
+app.post("/userData", cors(), auth, async (req, res) => {
+    const email = req.user.email
+    const user = await User.findOne({ email });
+    res.status(200).send({'message': `Welcome to your account ${user.first_name} ${user.last_name}`});
 });
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
+const cors = require('cors');
 
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
@@ -9,3 +10,7 @@ const port = process.env.PORT || API_PORT;
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+app.use(cors({
+    origin: '*'
+}));
